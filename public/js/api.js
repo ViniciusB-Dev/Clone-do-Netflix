d132cc3ffd231ad4659056a11fa4f03575ecc62e
Vinicius Barbosa <viniciusds.barbosa@hotmail.com>
2022-12-13 08:08:13 -0300

    updando clone

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "Key-do-site-tmdb-aque"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
